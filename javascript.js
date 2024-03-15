e673e5321af73c800a551f1738150eae42324c41
ericc <67475344+EricThunder@users.noreply.github.com>
2024-03-14 20:28:46 -0400

    Added whois API

--- javascript.js
+++ javascript.js
@@ -2,9 +2,11 @@ async function processInput() {
   let url = document.getElementById('entered_url').value;
   let cleanedUrl = cleanUrl(url);
   let isNonLatin = checkNonLatinCharacters(cleanedUrl);
+  let whoisData = whois_lookup(cleanedUrl);
 
   document.getElementById('clean_url').innerText = cleanedUrl;
   document.getElementById('non_latin').innerText = isNonLatin ? "The URL contains non-latin characters." : "The URL does not contain non-latin characters.";
+//  document.getElementById('whois_result').innerText = whoisData;
 
 
   // Clean the URL to remove any unnecessary parts
@@ -17,7 +19,6 @@ async function processInput() {
   url = url.split(":")[0];
   return url;
 }
-
 
 // Non-latin characters are characters that are not part of the ASCII character set (characters 0-127).
 function checkNonLatinCharacters(url) {
@@ -50,7 +51,6 @@ function checkNonLatinCharacters(url) {
     });
 
     const data = await response.json();
-    console.log(data); // Log the response to the console
     Display_GoogleSafeBrowsing_Results(data);
   } catch (error) {
     console.error('Error:', error);
@@ -65,7 +65,6 @@ function Display_GoogleSafeBrowsing_Results(data) {
   let GoogleSafeBrowsing_Result = '';
   if (data.matches && data.matches.length > 0) {
     GoogleSafeBrowsing_Result = '<p>Threat detected!</p>';
-    // If you want to display more information about the threat, you can loop through the matches array
     data.matches.forEach(({platformType, threat, threatEntryType, threatType}) => {
       GoogleSafeBrowsing_Result += `<p>Threat type: ${threatType}</p>`;
       GoogleSafeBrowsing_Result += `<p>Platform type: ${platformType}</p>`;
@@ -76,6 +75,25 @@ function Display_GoogleSafeBrowsing_Results(data) {
     GoogleSafeBrowsing_Result = '<p>No threat detected.</p>';
   }
   resultsContainer.innerHTML = GoogleSafeBrowsing_Result;
+}
+
+async function whois_lookup(url) {
+  const apiKey = 'at_Qy1xr2IAdN2t99QITjaV6pz6LWfm0'; // Replace with your actual API key
+  const apiUrl = 'https://www.whoisxmlapi.com/whoisserver/WhoisService';
+    try {
+        const response = await fetch(`${apiUrl}?domainName=${url}&apiKey=${apiKey}&outputFormat=JSON`);
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        const creationDate = data.WhoisRecord.createdDate;
+        document.getElementById('whois_result').textContent = `Domain Creation Date: ${creationDate}`;
+        //console.log('Domain Creation Date:', creationDate);
+        return creationDate;
+    } catch (error) {
+        console.error('Error:', error);
+        return 'Error';
+    }
 }
 
 
